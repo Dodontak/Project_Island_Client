44d1391d3f6195e6f8c70636b225e7a1373e6376
Dodontak <110716928+Dodontak@users.noreply.github.com>
2026-04-26 14:24:48 +0900

    Feat: Protobuf 21.12 추가
    21.12버전의 protobuf를 추가하고, 프로토버프가 잘 되는지 테스트 하기 위해 MyPawn 클래스와 블루프린트를 추가함.

---
+++ Source/Client/MyPawn.cpp
@@ -0,0 +1,40 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MyPawn.h"
+
+#include "Protocol.pb.h"
+
+// Sets default values
+AMyPawn::AMyPawn()
+{
+ 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AMyPawn::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	Protocol::C_CHAT pkt;
+	pkt.set_msg("Hello world!");
+	
+	UE_LOG(LogTemp, Display, TEXT("%s"), *FString(UTF8_TO_TCHAR(pkt.msg().c_str())));
+}
+
+// Called every frame
+void AMyPawn::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void AMyPawn::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
---
+++ Source/Client/ServerPacketHandler.h
@@ -0,0 +1,84 @@
+#pragma once
+
+#include "Types.h"
+#include "Session.h"
+#include "Protocol.pb.h"
+#include "SendBuffer.h"
+#include <memory>
+#include <functional>
+
+extern function<bool(function<void()>&, PacketSessionRef&, BYTE*, int32)> GPacketHandler[UINT16_MAX];
+
+enum : uint16
+{
+	PKT_C_LOGIN = 1000,
+	PKT_S_LOGIN = 1001,
+	PKT_C_ENTER_ROOM = 1002,
+	PKT_S_ENTER_ROOM = 1003,
+	PKT_C_CHAT = 1004,
+	PKT_S_CHAT = 1005,
+};
+
+bool	Handle_INVALID(function<void()>& outFunc, PacketSessionRef& session, BYTE* buffer, int32 len);
+void	Handle_S_LOGIN(const PacketSessionRef& session, const Protocol::S_LOGIN& pkt);
+void	Handle_S_ENTER_ROOM(const PacketSessionRef& session, const Protocol::S_ENTER_ROOM& pkt);
+void	Handle_S_CHAT(const PacketSessionRef& session, const Protocol::S_CHAT& pkt);
+
+class ServerPacketHandler
+{
+public:
+	static void	Init()
+	{
+		for (int i = 0; i < UINT16_MAX; ++i)
+			GPacketHandler[i] = Handle_INVALID;
+		GPacketHandler[PKT_S_LOGIN] = [](function<void()>& outFunc, PacketSessionRef& session, BYTE* buffer, int32 len) {
+			return GetCallback<Protocol::S_LOGIN>(outFunc, Handle_S_LOGIN, session, buffer, len);
+		};
+		GPacketHandler[PKT_S_ENTER_ROOM] = [](function<void()>& outFunc, PacketSessionRef& session, BYTE* buffer, int32 len) {
+			return GetCallback<Protocol::S_ENTER_ROOM>(outFunc, Handle_S_ENTER_ROOM, session, buffer, len);
+		};
+		GPacketHandler[PKT_S_CHAT] = [](function<void()>& outFunc, PacketSessionRef& session, BYTE* buffer, int32 len) {
+			return GetCallback<Protocol::S_CHAT>(outFunc, Handle_S_CHAT, session, buffer, len);
+		};
+	}
+
+	static bool	PacketHandler(function<void()>& outFunc, PacketSessionRef session, BYTE* buffer, int32 len)
+	{
+		PacketHeader*	header = reinterpret_cast<PacketHeader*>(buffer);
+		return GPacketHandler[header->id](outFunc, session, buffer, len);
+	}
+	static SendBufferRef MakeSendBuffer(Protocol::C_LOGIN& pkt) { return MakeSendBuffer(pkt, PKT_C_LOGIN); }
+	static SendBufferRef MakeSendBuffer(Protocol::C_ENTER_ROOM& pkt) { return MakeSendBuffer(pkt, PKT_C_ENTER_ROOM); }
+	static SendBufferRef MakeSendBuffer(Protocol::C_CHAT& pkt) { return MakeSendBuffer(pkt, PKT_C_CHAT); }
+
+private:
+	template<typename PacketType, typename ProcessFunc>
+	static bool	GetCallback(function<void()>& outFunc, ProcessFunc func, PacketSessionRef session, BYTE* buffer, int32 len)
+	{
+		PacketType	pkt;
+		if (false == pkt.ParseFromArray(buffer + sizeof(PacketHeader), len - sizeof(PacketHeader)))
+			return false;
+		outFunc = [func, session, pkt](){ func(session, pkt); };
+		return true;
+	}
+
+	template<typename T>
+	static SendBufferRef	MakeSendBuffer(T& pkt, uint16 pktId)
+	{
+		int	headerSize = sizeof(PacketHeader);
+		int	pktSize = pkt.ByteSizeLong();
+		SendBufferRef	sendBuffer = make_shared<SendBuffer>(headerSize + pktSize);
+
+		PacketHeader	header;
+		header.id = pktId;
+		header.size = headerSize + pktSize;
+
+		sendBuffer->AppendBuffer(reinterpret_cast<BYTE*>(&header), headerSize);
+		if (pktSize > 0)
+		{
+			pkt.SerializeToArray(sendBuffer->WritePos(), pktSize);
+			sendBuffer->OnWrite(pktSize);
+		}
+		return sendBuffer;
+	}
+};
